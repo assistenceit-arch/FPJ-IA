2db3047df01b80e8af2500e3f1a4442bbca66a96
MIKLOX <assistence.it@gmail.com>
2026-06-30 11:22:27 -0500

    chore: configure environment and prisma

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 
 import configuration from './config/app.config';
+import { environmentValidationSchema } from './config/environment.validation';
 import { AppController } from './app.controller';
 
 @Module({
@@ -9,6 +10,7 @@ import { AppController } from './app.controller';
     ConfigModule.forRoot({
       isGlobal: true,
       load: [configuration],
+      validationSchema: environmentValidationSchema,
     }),
   ],
   controllers: [AppController],
